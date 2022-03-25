145a251a87afad0e7a57139d5fbedc759ef73091
stellantilet <stellantilet@gmail.com>
2022-03-25 08:09:47 -0400

    fixed project

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ initializeDB()
 
 // Express configuration
 app.use(cors())
-app.set('port', process.env.PORT || 5000)
+app.set('port', process.env.PORT || 80)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
